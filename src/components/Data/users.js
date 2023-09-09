279683648de7f00211182609dd16284913924b28
Maciej Waszkiewicz <macieq091@gmail.com>
2023-09-09 17:47:13 +0200

    users list component

---
+++ src/components/Data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Maciej Waszkiewicz',
+    attendence: '45%',
+    avarage: '2.3',
+  },
+  {
+    name: 'Magda Gwiazdowska',
+    attendence: '55%',
+    avarage: '3.1',
+  },
+  {
+    name: 'Lidia Waszkiewicz',
+    attendence: '25%',
+    avarage: '2.1',
+  },
+  {
+    name: 'Jarosław Waszkiewicz',
+    attendence: '42%',
+    avarage: '2.2',
+  },
+  {
+    name: 'Tomasz Nowak',
+    attendence: '67%',
+    avarage: '4.5',
+  },
+];

5a8d6bb08c2341c117bfa2cbbd232f2993c11976
Maciej Waszkiewicz <macieq091@gmail.com>
2023-09-13 17:56:01 +0200

    component life cycle

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledButton = styled.button`
   width: 25px;
   height: 25px;
+  margin: 10px;
   background-color: ${({ theme }) => theme.colors.grey};
   border-radius: 50px;
   border: none;

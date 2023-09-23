79b6c320552a1de228d87a069a20ca56e474d0bf
Maciej Waszkiewicz <macieq091@gmail.com>
2023-09-23 11:00:55 +0200

    custom hook created

--- src/components/templates/MainTemplates/MainTemplates.js
+++ src/components/templates/MainTemplates/MainTemplates.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Navigation from '../../organisms/Navigation/Navigation';
 import { Wrapper } from './MainTemplates.styles';
 
+const SearchBar = styled.div`
+grid-row: 1 / 1;
+grid-column: 2 / 3;
+`
+
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
